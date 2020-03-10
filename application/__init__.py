a29b4f3ed29b975672c8c31cbfefb0d391b60432
martaniblett <martaniblett@gmail.com>
2020-03-10 16:47:48 +0000

    Flask forms files

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,17 @@
 # import Flask class from the flask module
 from flask import Flask
-# create a new instance of Flask and store it in app 
+#import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+
+
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']=getenv('DATABASE_URI')
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
+db = SQLAlchemy(app)
 # import the ./application/routes.py file
 from application import routes
+
+
--- application/routes.py
+++ application/routes.py
@@ -2,6 +2,15 @@
 from flask import render_template
 # import the app object from the ./application/__init__.py
 from application import app
+from application.models import Posts
+
+#forms:
+from flask import render_template, redirect, url_for
+from application import app, db
+from application.models import Posts
+from application.forms import PostForm
+
+
 # define routes for / & /home, this function will be called when these are accessed
 blogData = [
     {  
@@ -18,7 +27,8 @@ blogData = [
 @app.route('/')
 @app.route('/home')
 def home():
- return render_template('home.html', title='Home Page', posts=blogData)
+ postData = Posts.query.all()
+ return render_template('home.html', title='Home Page', post=postData)
 
 @app.route('/about')
 def about():
@@ -27,3 +37,24 @@ def about():
 @app.route('/login')
 def login():
  return render_template('login.html', title='Login Page')
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    form = PostForm()
+    if form.validate_on_submit():
+        postData = Posts(
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            title=form.title.data,
+            content=form.content.data
+        )
+
+        db.session.add(postData)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+
+    else:
+        print(form.errors)
+
+    return render_template('post.html', title='Post', form=form)

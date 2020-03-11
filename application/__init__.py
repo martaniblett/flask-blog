47b2091ab1386284551b439c5f41df193ad7762a
martaniblett <martaniblett@gmail.com>
2020-03-11 16:45:21 +0000

    Flask app register and login

--- application/__init__.py
+++ application/__init__.py
@@ -1,17 +1,21 @@
-# import Flask class from the flask module
 from flask import Flask
-#import SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
-
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
 app.config['SQLALCHEMY_DATABASE_URI']=getenv('DATABASE_URI')
 app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 db = SQLAlchemy(app)
-# import the ./application/routes.py file
+
 from application import routes
+
+
 
 
